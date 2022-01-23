507fe8bc0e3d9f7d67fd318b84780ce6492872ab
karybeeDIcode <philtian@buaa.edu.cn>
2022-01-23 12:31:41 +0800

    Initial Commit

---
+++ xcodeTryGit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcodeTryGit
+//
+//  Created by karybee on 2022/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
